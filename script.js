f34b62168442d3015013f5645ede9c286475ee0b
Shivanand1600 <ishivanand2002@gmail.com>
2024-03-07 21:51:10 +0530

    Created About section in portfolio

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let toggleMid = document.getElementById("toggleMid")
 let toggleOut = document.getElementById("toogleOuter")
-let navBtn=document.querySelectorAll(".navBtns")
-let resBtn=document.getElementsByClassName("button")
+// let navBtn=document.querySelectorAll(".navBtns")
+// let resBtn=document.getElementsByClassName("button")
+let main=document.getElementById("mainSection")
 
 function night() {
     toggleMid.style.transform = "translateX(35px)"
     toggleMid.style.backgroundImage = `url(./images/moon.png)`
     toggleOut.style.backgroundImage = "url(./images/night.jpeg)"
     document.body.classList.toggle('dark-mode');
+    main.style.boxShadow="10px 10px 10px white"
 }
 function day() {
     toggleMid.style.transform = "translateX(0px)"
