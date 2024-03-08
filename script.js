3c11355f022317a816b85162820d90e21bf78302
Shivanand1600 <ishivanand2002@gmail.com>
2024-03-08 22:36:00 +0530

    Completed HTML & CSS Part

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let toggleMid = document.getElementById("toggleMid")
 let toggleOut = document.getElementById("toogleOuter")
-// let navBtn=document.querySelectorAll(".navBtns")
-// let resBtn=document.getElementsByClassName("button")
+let navBtn=document.getElementsByClassName("navBtns")
+let resBtn=document.getElementsByClassName("button")
 let main=document.getElementById("mainSection")
 
 function night() {
@@ -10,12 +10,20 @@ function night() {
     toggleOut.style.backgroundImage = "url(./images/night.jpeg)"
     document.body.classList.toggle('dark-mode');
     main.style.boxShadow="10px 10px 10px white"
+    navBtn.forEach((buttons)=>{
+        buttons.style.color="white"
+    })
+    resBtn[0].style.color="white"
 }
 function day() {
     toggleMid.style.transform = "translateX(0px)"
     toggleMid.style.backgroundImage = `url(./images/sun.png)`
     toggleOut.style.backgroundImage = "url(./images/clouds.jpg)"
     document.body.classList.toggle('light-mode');
+    navBtn.forEach((buttons)=>{
+        buttons.style.color="black"
+    })
+    resBtn[0].style.color="black"
 }
 
 let count = 0;
@@ -29,4 +37,26 @@ function toggle() {
         count = 0;
     }
 }
+
+
+
+ //NAVBAR-MID-BUTTONS-SCROLL-TO POSITION
+ navBtn.forEach((btn)=>{
+  btn[0].addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+btn[1].addEventListener("click", () => {
+    window.scrollTo({ top: 140, behavior: 'smooth' });
+})
+btn[2].addEventListener("click", () => {
+    window.scrollTo({ top: 2420, behavior: 'smooth' });
+})
+btn[3].addEventListener("click", () => {
+    window.scrollTo({ top: 3200, behavior: 'smooth' });
+})
+btn[4].addEventListener("click", () => {
+  window.scrollTo({ top: 3200, behavior: 'smooth' });
+})
+ })
+
 
